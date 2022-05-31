feb8b1af40ca8bc9366e24c36d755f998df74aa0
Jakub Vel <34968650+trivarialthea@users.noreply.github.com>
2022-05-31 11:50:04 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull ghcr.io/trivarialthea/dockernginxexample:latest
-docker run -d -p 80:80 -p 22:22 --name dockernginxexample trivarialthea/dockernginxexample
+docker run -d -p 80:80 -p 22:22 --name dockernginxexample ghcr.io/trivarialthea/dockernginxexample
