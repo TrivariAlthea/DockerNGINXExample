99eac0a85ba645943fbdf231ae19561e37275943
jtihlarik <jtihlarik@ssipf.cz>
2022-06-02 09:53:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull ghcr.io/jtihlarik/dockernginxexample:latest
-docker run -d -p 80:80 -p 22:22 --name dockernginxexample ghcr.io/trivarialthea/dockernginxexample
+docker run -d -p 80:80 -p 22:22 --name dockernginxexample ghcr.io/jtihlarik/dockernginxexample
