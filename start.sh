a69ec34ab6b3201001db27126d4b8de6be7a77f3
jtihlarik <jtihlarik@ssipf.cz>
2022-06-02 10:12:23 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 
 docker pull ghcr.io/jtihlarik/dockernginxexample:latest
 docker run -d -p 80:80 -p 22:22 --name dockernginxexample ghcr.io/jtihlarik/dockernginxexample
+docker container inspect dockernginxexample | grep -i IPAddress
